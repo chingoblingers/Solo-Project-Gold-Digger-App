8f28fff4f1c4aa8bbb34dffed146fae529007013
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-11-24 00:06:47 -0600

    created functions for server sent event logic and added to new else if statment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import fs from "node:fs/promises"
 import path from "node:path"
 import mime from "mime"
 
+let currentprice = 600
+
 const PORT = 6000
 
 const __dirname = import.meta.dirname
@@ -13,6 +15,19 @@ let filePath = ""
 
 if(req.url === "/"){
 filePath = path.join(publicFolder, "index.html")
+}else if (req.url === "/stream") {
+ res.statusCode = 200   
+ res.setHeader("Content-Type", "event/stream")
+ res.setHeader("Cache-Control", "no-cache")
+ res.setHeader("Connection", "keep-alive")
+
+setInterval(() => {
+    currentprice = getNextPrice(currentprice)
+    res.write(
+        ` data: ${JSON.stringify({event: "gold-price-update", price: currentprice})}\n\n`
+    )
+}, 3000);
+
 }else {
 filePath = path.join(publicFolder, req.url)
 }
@@ -45,4 +60,12 @@ res.end(readError)
 })
 
 server.listen(PORT, ()=> console.log(`running on PORT: ${PORT} `))
+
+
+function getNextPrice(price){
+
+let change = Math.floor(Math.random()*317 -158)
+let newPrice = price + change
+return newPrice
+}
 
